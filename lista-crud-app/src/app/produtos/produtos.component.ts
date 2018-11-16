9f15dfb5d2d1ed478448f8380d5d0e1062c506c6
I4PROINFO\anmak <andrew.mak@i4pro.com.br>
2018-11-16 14:05:00 -0200

    layout novo produto e editar produto

--- lista-crud-app/src/app/produtos/produtos.component.ts
+++ lista-crud-app/src/app/produtos/produtos.component.ts
@@ -1,22 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+import { Produto } from 'src/model/produto';
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+
+const ELEMENT_DATA: Produto[] = [
+  {id: 1, nome_produto: 'Hydrogen',preco_produto: 1.0079, desc_produto: 'H', dt_atualizacao: new Date()},
+  {id: 2, nome_produto: 'Helium', preco_produto: 4.0026, desc_produto: 'He', dt_atualizacao: new Date()},
+  {id: 3, nome_produto: 'Lithium', preco_produto: 6.941, desc_produto: 'Li', dt_atualizacao: new Date()},
+  {id: 4, nome_produto: 'Beryllium', preco_produto: 9.0122, desc_produto: 'Be', dt_atualizacao: new Date()},
+  {id: 5, nome_produto: 'Boron', preco_produto: 10.811, desc_produto: 'B', dt_atualizacao: new Date()},
+  {id: 6, nome_produto: 'Carbon', preco_produto: 12.0107, desc_produto: 'C', dt_atualizacao: new Date()},
+  {id: 7, nome_produto: 'Nitrogen', preco_produto: 14.0067, desc_produto: 'N', dt_atualizacao: new Date()},
+  {id: 8, nome_produto: 'Oxygen', preco_produto: 15.9994, desc_produto: 'O', dt_atualizacao: new Date()},
+  {id: 9, nome_produto: 'Fluorine', preco_produto: 18.9984, desc_produto: 'F', dt_atualizacao: new Date()},
+  {id: 10, nome_produto: 'Neon', preco_produto: 20.1797, desc_produto: 'Ne', dt_atualizacao: new Date()}
 ];
 @Component({
   selector: 'app-produtos',
