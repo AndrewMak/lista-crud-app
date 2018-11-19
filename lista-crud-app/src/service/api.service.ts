badb4bf5c0862013319ca0e210b70d23f2ae566c
I4PROINFO\anmak <andrew.mak@i4pro.com.br>
2018-11-19 12:35:23 -0200

    adicionando service de acesso a REST API da nossa app

---
+++ lista-crud-app/src/service/api.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+import { Produto } from 'src/model/produto';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+const apiUrl = "http://localhost:3000/api/produtos";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getProdutos (): Observable<Produto[]> {
+    return this.http.get<Produto[]>(apiUrl)
+      .pipe(
+        tap(produtos => console.log('leu os produtos')),
+        catchError(this.handleError('getProdutos', []))
+      );
+  }
+
+  getProduto(id: number): Observable<Produto> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.get<Produto>(url).pipe(
+      tap(_ => console.log(`leu o produto id=${id}`)),
+      catchError(this.handleError<Produto>(`getProduto id=${id}`))
+    );
+  }
+
+  addProduto (produto): Observable<Produto> {
+    return this.http.post<Produto>(apiUrl, produto, httpOptions).pipe(
+      tap((produto: Produto) => console.log(`adicionou o produto com w/ id=${produto.id}`)),
+      catchError(this.handleError<Produto>('addProduto'))
+    );
+  }
+
+  updateProduto(id, produto): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.put(url, produto, httpOptions).pipe(
+      tap(_ => console.log(`atualiza o produco com id=${id}`)),
+      catchError(this.handleError<any>('updateProduto'))
+    );
+  }
+
+  deleteProduto (id): Observable<Produto> {
+    const url = `${apiUrl}/${id}`;
+
+    return this.http.delete<Produto>(url, httpOptions).pipe(
+      tap(_ => console.log(`remove o produto com id=${id}`)),
+      catchError(this.handleError<Produto>('deleteProduto'))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error);
+
+      return of(result as T);
+    };
+  }
+}
